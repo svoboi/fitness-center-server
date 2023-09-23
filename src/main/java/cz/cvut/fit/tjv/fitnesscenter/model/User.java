84592ba82b1789eb9ffedcfbdf02883342b7c13c
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-09-23 20:18:41 +0200

    Add username and password for User

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
@@ -28,6 +28,9 @@ public class User {
     private String firstName;
     private String lastName;
 
+    private String username;
+    private String password;
+
     @Email
     private String emailAddress;
     private String phoneNumber;
