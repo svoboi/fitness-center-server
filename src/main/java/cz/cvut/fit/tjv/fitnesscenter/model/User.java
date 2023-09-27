f4469c105f0fcd00501b85863d792b77a3439ec1
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-09-27 09:17:00 +0200

    Add NotBlank and NotNull constraints to User

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
@@ -1,12 +1,12 @@
 package cz.cvut.fit.tjv.fitnesscenter.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.HashSet;
@@ -21,17 +21,25 @@ public class User {
     @GeneratedValue
     public Long id;
 
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
 
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 
     @Email
+    @NotBlank
     private String emailAddress;
+    @NotBlank
     private String phoneNumber;
 
+    @NotNull
     private Boolean employee;
+    @NotNull
     private Boolean customer;
 
     @ManyToMany(mappedBy = "trainers")
