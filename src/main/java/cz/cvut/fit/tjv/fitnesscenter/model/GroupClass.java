18a83614533caacc2de0c363e33c496969e87495
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-10-05 14:38:49 +0200

    Delete toString by lombok, fixes debuggers circular reference with users and classes

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/GroupClass.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/GroupClass.java
@@ -2,7 +2,10 @@ package cz.cvut.fit.tjv.fitnesscenter.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -13,7 +16,6 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class GroupClass {
     @Id
     @GeneratedValue
--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/model/User.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,7 +17,6 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class User {
     @Id
     @GeneratedValue
