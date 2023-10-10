9bf1eb3407b3ab7a1d8f0e2098e6dac00c0691b4
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-10-10 22:26:18 +0200

    Fix error messages

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/controller/ControllerAdvisor.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/controller/ControllerAdvisor.java
@@ -47,7 +47,7 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("message", e.getMessage() + " not found.");
+        body.put("message", e.getMessage() + " entity not found.");
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
@@ -69,7 +69,7 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("message", "This room doesn't have enough much space.");
+        body.put("message", "This room doesn't have enough space.");
 
         return new ResponseEntity<>(body, HttpStatus.CONFLICT);
     }
