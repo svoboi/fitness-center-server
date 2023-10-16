dec82c549a9813bc6e7987579c05d5530873bb81
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-10-16 19:30:33 +0200

    Add function for finding all classes by trainer around given interval

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/dao/GroupClassRepository.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/dao/GroupClassRepository.java
@@ -3,12 +3,19 @@ package cz.cvut.fit.tjv.fitnesscenter.dao;
 
 import cz.cvut.fit.tjv.fitnesscenter.model.GroupClass;
 import cz.cvut.fit.tjv.fitnesscenter.model.Room;
+import cz.cvut.fit.tjv.fitnesscenter.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Repository
 public interface GroupClassRepository extends CrudRepository<GroupClass, Long> {
     Collection<GroupClass> findAllByRoom(Room room);
+
+    Collection<GroupClass> findAllByTimeFromBetweenOrTimeToBetweenAndTrainersContaining
+            (LocalDateTime timeFromFrom, LocalDateTime timeFromTo,
+             LocalDateTime timeToFrom, LocalDateTime timeToTo,
+             User user);
 }
