081d29d34697d6bd756a2ffc2c17ea2d623e74b4
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-10-16 20:47:40 +0200

    Add endpoint for counting worked hours in given interval for given employee

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/controller/UserController.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/controller/UserController.java
@@ -1,10 +1,16 @@
 package cz.cvut.fit.tjv.fitnesscenter.controller;
 
 import cz.cvut.fit.tjv.fitnesscenter.business.UserService;
+import cz.cvut.fit.tjv.fitnesscenter.controller.dto.TimeFrameDTO;
 import cz.cvut.fit.tjv.fitnesscenter.controller.dto.UserMapper;
 import cz.cvut.fit.tjv.fitnesscenter.model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -13,4 +19,12 @@ public class UserController extends AbstractController<User> {
         super(userService, userMapper);
     }
 
+    @GetMapping("/hours/{id}")
+    public ResponseEntity<Map<String, Object>> allClassesByRoom(@RequestBody @Validated TimeFrameDTO timeFrame, @PathVariable Long id) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("hours", ((UserService) service).countHoursByUserAndMonth(id, timeFrame.getTimeFrom(), timeFrame.getTimeTo()));
+//        body.put("timeFrom", timeFrame.getTimeFrom());
+//        body.put("timeTo", timeFrame.getTimeTo());
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 }
