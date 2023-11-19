c613946315221c961f05e62259896ead7c21e6de
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-11-19 22:30:43 +0100

    Fix JPQL for finding all groupclasses in room within timeframe

--- src/main/java/cz/cvut/fit/tjv/fitnesscenter/dao/GroupClassRepository.java
+++ src/main/java/cz/cvut/fit/tjv/fitnesscenter/dao/GroupClassRepository.java
@@ -17,7 +17,7 @@ public interface GroupClassRepository extends CrudRepository<GroupClass, Long> {
 
     Collection<GroupClass> findAllByTrainersContaining(User user);
 
-    @Query("SELECT g FROM GroupClass g WHERE ?1 member of g.trainers AND (g.timeFrom BETWEEN ?2 and ?3 OR g.timeTo BETWEEN ?2 and ?3)")
+    @Query("SELECT g FROM GroupClass g WHERE g.room = ?1 AND (g.timeFrom BETWEEN ?2 and ?3 OR g.timeTo BETWEEN ?2 and ?3)")
     Collection<GroupClass> findAllByRoomAndTime(Room room, LocalDateTime timeFrom, LocalDateTime timeTo);
 
     @Query("SELECT g FROM GroupClass g WHERE ?1 member of g.trainers AND (g.timeFrom BETWEEN ?2 and ?3 OR g.timeTo BETWEEN ?2 and ?3)")
